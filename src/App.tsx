5ef1ec002ec76d95198e53506cc0974a2b9b5940
Bhargav <93113682+bhargavd-hkis@users.noreply.github.com>
2024-12-27 17:01:27 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 // @ts-nocheck
-import React, { useEffect, useRef, useState } from 'react';
-import Nouislider from 'nouislider-react';
-import 'nouislider/distribute/nouislider.css';
-import './App.css';
 
-import { fetchFile } from '@ffmpeg/util';
+import React, { useEffect, useRef, useState } from "react";
+import Nouislider from "nouislider-react";
+import "nouislider/distribute/nouislider.css";
+import "./App.css";
 
 let ffmpeg;
 
@@ -13,36 +12,33 @@ function App() {
   const [videoDuration, setVideoDuration] = useState(0);
   const [endTime, setEndTime] = useState(0);
   const [startTime, setStartTime] = useState(0);
-  const [videoSrc, setVideoSrc] = useState('');
+  const [videoSrc, setVideoSrc] = useState("");
   const [videoFile, setVideoFile] = useState(null);
   const [isScriptLoaded, setIsScriptLoaded] = useState(false);
-  const [videoTrimmedUrl, setVideoTrimmedUrl] = useState('');
+  const [videoTrimmedUrl, setVideoTrimmedUrl] = useState("");
   const videoRef = useRef();
 
-  const handleFileUpload = (event) => {
-    const file = event.target.files[0];
-    const blobURL = URL.createObjectURL(file);
-    setVideoFile(file);
-    setVideoSrc(blobURL);
-  };
-
+  // Convert the time obtained from the video to HH:MM:SS format
   const convertToHHMMSS = (val) => {
     const secNum = parseInt(val, 10);
     let hours = Math.floor(secNum / 3600);
     let minutes = Math.floor((secNum - hours * 3600) / 60);
     let seconds = secNum - hours * 3600 - minutes * 60;
 
-    if (hours < 10) hours = '0' + hours;
-    if (minutes < 10) minutes = '0' + minutes;
-    if (seconds < 10) seconds = '0' + seconds;
+    if (hours < 10) hours = "0" + hours;
+    if (minutes < 10) minutes = "0" + minutes;
+    if (seconds < 10) seconds = "0" + seconds;
 
-    return hours === '00' ? `${minutes}:${seconds}` : `${hours}:${minutes}:${seconds}`;
+    return hours === "00"
+      ? `${minutes}:${seconds}`
+      : `${hours}:${minutes}:${seconds}`;
   };
 
   useEffect(() => {
     const loadFFmpegScript = async () => {
-      const script = document.createElement('script');
-      script.src = 'https://cdn.jsdelivr.net/npm/@ffmpeg/ffmpeg@0.11.2/dist/ffmpeg.min.js';
+      const script = document.createElement("script");
+      script.src =
+        "https://cdn.jsdelivr.net/npm/@ffmpeg/ffmpeg@0.11.2/dist/ffmpeg.min.js";
       script.onload = async () => {
         ffmpeg = window.FFmpeg.createFFmpeg({ log: true });
         await ffmpeg.load();
@@ -65,7 +61,7 @@ function App() {
   }, [videoSrc]);
 
   const updateOnSliderChange = (values, handle) => {
-    setVideoTrimmedUrl('');
+    setVideoTrimmedUrl("");
     let readValue = Math.floor(values[handle]);
 
     if (handle === 1) {
@@ -86,12 +82,80 @@ function App() {
     }
   };
 
-  const handleTrim = async () => {
-    if (!isScriptLoaded || !videoFile) {
-      console.error("FFmpeg script not loaded or no video file provided.");
-      return;
+  const handleFileUpload = (event) => {
+    const file = event.target.files[0];
+
+    if (file) {
+      const blobURL = URL.createObjectURL(file);
+      setVideoFile(file);
+      setVideoSrc(blobURL);
+    } else {
+      alert("Please select file first");
     }
-  
+  };
+
+  const chunkTheVideo = async (
+    videoFile,
+    videoFileType,
+    chunkSize = 50 * 1024 * 1024
+  ) => {
+    const totalChunks = Math.ceil(videoFile.size / chunkSize),
+      chunkFiles = [];
+    let currPosition = 0;
+
+    for (let i = 0; i < totalChunks; i++) {
+      const chunk = videoFile.slice(currPosition, currPosition + chunkSize);
+      currPosition += chunkSize;
+
+      // Calculate the chunk's start and end times based on the video duration
+      const chunkStartTime = (i / totalChunks) * videoDuration;
+      const chunkEndTime = ((i + 1) / totalChunks) * videoDuration;
+
+      // Include only chunks that intersect with the trimming range
+      // if (chunkStartTime < endTime && chunkEndTime > startTime) {
+      const arrayBuffer = await chunk.arrayBuffer();
+      const chunkName = `chunk-${i}.${videoFileType}`;
+      ffmpeg.FS("writeFile", chunkName, new Uint8Array(arrayBuffer));
+      chunkFiles.push(chunkName);
+      // }
+    }
+
+    return chunkFiles;
+  };
+
+  const concateUint8Arrays = (arrays) => {
+    const totalLength = arrays.reduce((sum, arr) => sum + arr.length, 0);
+    const combined = new Uint8Array(totalLength);
+    let offset = 0;
+    for (const arr of arrays) {
+      combined.set(arr, offset);
+      offset += arr.length;
+    }
+    return combined;
+  };
+
+  const concatVideoChunks = async (chunksFiles, videoFileType) => {
+    if (chunksFiles.length === 0) {
+      console.log("No chunks files is concatenate");
+    }
+
+    try {
+      const chunkDataArray = await Promise.all(
+        chunksFiles.map((fileName) => ffmpeg.FS("readFile", fileName))
+      );
+
+      const combinedArray = concateUint8Arrays(chunkDataArray);
+
+      const combinedFileName = `combined-video.${videoFileType}`;
+      ffmpeg.FS("writeFile", combinedFileName, combinedArray);
+
+      return combinedFileName;
+    } catch (error) {
+      console.log({ error });
+    }
+  };
+
+  const handleTrim = async () => {
     try {
       setIsLoading(true);
 
@@ -152,12 +216,12 @@ function App() {
         setVideoTrimmedUrl(URL.createObjectURL(finalVideo));
       }
     } catch (error) {
-      console.error("Error while trimming video:", error);
+      console.error("Error in handleTrim:", error);
     } finally {
       setIsLoading(false);
     }
   };
-  
+
   return (
     <div
       style={{
